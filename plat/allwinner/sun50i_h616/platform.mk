1f20053692f3e5c682667d698b1d3031e948e8f9
Andre Przywara <andre.przywara@arm.com>
2020-11-28 01:39:17 +0000

    allwinner: Add Allwinner H616 SoC support
    The new Allwinner H616 SoC lacks the management controller and the secure
    SRAM A2, so we need to tweak the memory map quite substantially:
    We run BL31 in DRAM. Since the DRAM starts at 1GB, we cannot use our
    compressed virtual address space (max 256MB) anymore, so we revert to
    the full 32bit VA space and use a flat mapping througout all of it.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

---
+++ plat/allwinner/sun50i_h616/platform.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (c) 2017-2019, ARM Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+# The differences between the platform are covered by the include files.
+include plat/allwinner/common/allwinner-common.mk
+
+BL31_SOURCES		+=	drivers/allwinner/axp/axp805.c		\
+				drivers/mentor/i2c/mi2cv.c
+
+$(eval $(call add_define,SUNXI_RUN_IN_DRAM))
